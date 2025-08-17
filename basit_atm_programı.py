164c58909c54ffbb2d4acbe84d747f8fc86c616b
asminhelin <asminhelink1306@gmail.com>
2025-08-17 18:09:29 +0300

    Add simple ATM program using loop

---
+++ basit_atm_programı.py
@@ -0,0 +1,39 @@
+print("""***********************************
+ATM Makinesine Hoşgeldiniz.
+
+İşlemler
+1. Bakiye Sorgulama
+2. Para Yatırma
+3. Para Çekme
+
+Programdan çıkmak için 'q'ya basın.
+***********************************
+""")
+
+bakiye= 1000
+
+while True:
+    işlem=input("İşlemi seçiniz:")
+
+    if (işlem=="q"):
+        print("Yine Bekleriz.")
+        break
+
+
+    elif(işlem=="1"):
+        print("Bakiyeniz {} TL'dir.".format(bakiye))
+
+    elif(işlem=="2"):
+        miktar=int(input("Miktarı giriniz:"))
+        bakiye+=miktar
+
+    elif(işlem=="3"):
+        miktar=int(input("Miktarı giriniz:"))
+        if(bakiye-miktar<0):
+            print("Böyle bir miktar çekemezsiniz.")
+            continue
+        bakiye-=miktar
+        
+    else:
+        print("Geçersiz İşlem.")
+    
